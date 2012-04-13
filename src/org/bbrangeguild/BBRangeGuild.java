78d25db2a63f7da2aa7f4112c5bb04577026f0f3
Avery <bamboombamboom@hotmail.com>
2012-04-12 19:42:05 -0500

    Changes camera angle if range icon is in the way.

--- src/org/bbrangeguild/BBRangeGuild.java
+++ src/org/bbrangeguild/BBRangeGuild.java
@@ -3,21 +3,47 @@ package org.bbrangeguild;
 import org.bbrangeguild.strategy.CompeteStrategy;
 import org.bbrangeguild.strategy.EquipStrategy;
 import org.bbrangeguild.strategy.ShootStrategy;
+import org.powerbot.concurrent.Task;
+import org.powerbot.concurrent.strategy.Condition;
 import org.powerbot.concurrent.strategy.Strategy;
 import org.powerbot.game.api.ActiveScript;
 import org.powerbot.game.api.Manifest;
+import org.powerbot.game.api.methods.widget.Camera;
+import org.powerbot.game.api.util.Random;
 
-@Manifest(name = "BBRangeGuild", authors = "BOOM BOOM", version = 1.0D, description = "TODO")
+@Manifest(name = "BBRangeGuild", authors = "BOOM BOOM", version = 1.0D, description = "TODO", premium = true)
 public class BBRangeGuild extends ActiveScript {
+
+    //1184, 0 = out of shots
+    //1188, 3 = Yes for 200 coins
 
     @Override
     protected void setup() {
+        final Strategy camera = new Strategy(new Condition() {
+            @Override
+            public boolean validate() {
+                final int yaw = Camera.getYaw();
+                return Camera.getPitch() > 8 || (yaw < 320 && yaw > 303);
+            }
+        }, new Task() {
+            @Override
+            public void run() {
+                if (Camera.getPitch() > 8)
+                    Camera.setPitch(Random.nextInt(0, 9));
+                final int yaw = Camera.getYaw();
+                if (yaw < 320 && yaw > 303)
+                    Camera.setAngle(Random.nextBoolean() ? Random.nextInt(320, 335) : Random.nextInt(285, 300));
+            }
+        });
         final EquipStrategy equipStrategy = new EquipStrategy(this);
         final CompeteStrategy competeStrategy = new CompeteStrategy();
         final ShootStrategy shootStrategy = new ShootStrategy();
-        provide(new Strategy(equipStrategy));
-        provide(new Strategy(competeStrategy));
-        provide(new Strategy(shootStrategy));
+        camera.setLock(false);
+        competeStrategy.setReset(true);
+        provide(new Strategy(equipStrategy, equipStrategy));
+        provide(camera);
+        provide(competeStrategy);
+        provide(new Strategy(shootStrategy, shootStrategy));
     }
 
 }
