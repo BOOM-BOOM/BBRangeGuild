39584b1acd4d099ff91c2f25aed23608d9cbc75a
Avery <bamboombamboom@hotmail.com>
2012-08-06 16:17:46 -0500

    Improved item exchanging and added support for buying upto an amount.

--- src/org/bbrangeguild/strategy/ExchangeStrategy.java
+++ src/org/bbrangeguild/strategy/ExchangeStrategy.java
@@ -7,6 +7,7 @@ import org.powerbot.concurrent.strategy.Strategy;
 import org.powerbot.game.api.methods.Widgets;
 import org.powerbot.game.api.methods.interactive.NPCs;
 import org.powerbot.game.api.methods.interactive.Players;
+import org.powerbot.game.api.methods.tab.Inventory;
 import org.powerbot.game.api.methods.widget.Camera;
 import org.powerbot.game.api.util.Random;
 import org.powerbot.game.api.util.Time;
@@ -18,7 +19,6 @@ import org.powerbot.game.api.wrappers.widget.WidgetChild;
  */
 public class ExchangeStrategy extends Strategy implements Condition, Task {
 
-    private int child;
     private BBRangeGuild script;
 
     public ExchangeStrategy(final BBRangeGuild script) {
@@ -34,25 +34,22 @@ public class ExchangeStrategy extends Strategy implements Condition, Task {
     public void run() {
         if (Widgets.get(278, 0).visible() &&  Widgets.get(512, 0).visible()) {
             script.setStatus("Exchanging...");
-            for (final WidgetChild widgetChild : Widgets.get(512, 0).getChildren()) {
-                if (widgetChild.getChildId() == 1464 && script.getAmount() > 0 ? widgetChild.getChildStackSize() >= script.getAmount() : widgetChild.getChildStackSize() < 1020) {
-                    script.log.info("You do not have enough tickets to exchange.");
+            for (WidgetChild widgetChild : Widgets.get(512, 0).getChildren()) {
+                if (widgetChild.getChildId() == 1464 && (script.getAmount() > 0 ? maxAmount() : widgetChild.getChildStackSize() < script.getExchangeItem().getValue())) {
+                    script.log.info("You do not have enough tickets to exchange. You have: " + widgetChild.getChildStackSize());
                     script.stop();
                     return;
                 }
             }
 
-            if (script.getExchangeMode() == 0)
-                child = 0;
-            else if (script.getExchangeMode() == 1)
-                child = 2;
-            else if (script.getExchangeMode() == 2)
-                child = 5;
-
-            if (Widgets.get(278, 16).getChild(child).interact("Buy"))
-                Time.sleep(Random.nextInt(100, 300));
+            int count = getCount();
+            if (Widgets.get(278, 16).getChild(script.getExchangeItem().getChildId()).interact("Buy")) {
+                for (int i = 0; i < 20 && count == getCount(); i++)
+                    Time.sleep(100);
+            }
         } else {
             script.setStatus("Trading...");
+
             final NPC merchant = NPCs.getNearest(694);
             if (merchant != null) {
                 if (merchant.isOnScreen()) {
@@ -67,6 +64,21 @@ public class ExchangeStrategy extends Strategy implements Condition, Task {
                     Camera.turnTo(merchant);
             }
         }
+    }
+
+    private boolean maxAmount() {
+        for (WidgetChild widgetChild : Widgets.get(512, 0).getChildren()) {
+            if (widgetChild.getChildId() == script.getExchangeItem().getItemId())
+                return widgetChild.getChildStackSize() >= script.getAmount();
+        }
+        return false;
+    }
+
+    private int getCount() {
+        int count = 0;
+        for (WidgetChild widgetChild : Widgets.get(512, 0).getChildren())
+            count += widgetChild.getChildStackSize();
+        return count;
     }
 
 }
