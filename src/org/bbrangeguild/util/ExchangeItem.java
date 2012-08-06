256bb9439d3fa749d18c1cdc073fa78b3912b4b6
Avery <bamboombamboom@hotmail.com>
2012-08-06 16:15:34 -0500

    Class to represent exchange items.

--- src/org/bbrangeguild/util/ExchangeItem.java
+++ src/org/bbrangeguild/util/ExchangeItem.java
@@ -1,11 +1,28 @@
 package org.bbrangeguild.util;
 
 /**
- * Created with IntelliJ IDEA.
- * User: Avery
- * Date: 8/5/12
- * Time: 9:34 PM
- * To change this template use File | Settings | File Templates.
+ * @author BOOM BOOM
  */
 public class ExchangeItem {
+
+    private int childId, value, itemId;
+
+    public ExchangeItem (final int childId, final int value, final int itemId) {
+        this.childId = childId;
+        this.value = value;
+        this.itemId = itemId;
+    }
+
+    public int getChildId() {
+        return childId;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
 }
